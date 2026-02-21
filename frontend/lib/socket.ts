b02c3b682ed7465b61a8f6f2334ad86fe9863998
varunkasnia <varunkasnia73@gmail.com>
2026-02-21 15:31:46 +0530

    Update socket.ts

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const WS_URL = process.env.NEXT_PUBLIC_WS_URL || 'http://localhost:8000';
+const WS_URL = 'https://certifiacate-app-1.onrender.com/';
 
 let socket: Socket | null = null;
 
